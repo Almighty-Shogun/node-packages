a736b87b07dc700937b6904614120c2476247d13
Almighty-Shogun <alm1ghtyshogun1998@gmail.com>
2026-03-28 14:30:18 +0100

    Chore: Implement default locale-getter

--- packages/utils/src/initApplication.ts
+++ packages/utils/src/initApplication.ts
@@ -1,14 +1,15 @@
 import disableZoom from './disableZoom'
 import useDarkTheme from './useDarkTheme'
+import getCorrectLocale from './internal/getCorrectLocale'
 
 type ApplicationConfig = {
     locale?: string;
     isDarkTheme?: boolean;
     isZoomDisabled?: boolean;
-}
+};
 
 export default function (config: ApplicationConfig): void {
-    document.documentElement.setAttribute("lang", config.locale ?? "en");
+    document.documentElement.setAttribute("lang", getCorrectLocale(config.locale));
 
     useDarkTheme(config.isDarkTheme ?? false);
 
