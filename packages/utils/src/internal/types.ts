d3bf1acb0bf7cfed27cb051948132ad4918bed44
Almighty-Shogun <alm1ghtyshogun1998@gmail.com>
2026-03-28 14:27:33 +0100

    Chore: Removed weather utils

--- packages/utils/src/internal/types.ts
+++ packages/utils/src/internal/types.ts
@@ -1,28 +1,3 @@
-export type OpenMeteoResponse = {
-    current: {
-        time: string;
-        weather_code: number;
-        temperature_2m: number;
-        temperature_2m_min: number;
-        temperature_2m_max: number;
-    };
-    hourly: {
-        time: string[];
-        weather_code: number[];
-        temperature_2m: number[];
-        temperature_2m_min: number[];
-        temperature_2m_max: number[];
-    };
-    daily: {
-        time: string[];
-        sunrise: string[];
-        sunset: string[];
-        weather_code: number[];
-        temperature_2m_min: number[];
-        temperature_2m_max: number[];
-    }
-};
-
 export type TranslateExists = (key: string) => boolean;
 export type Translate = (key: string, params?: Record<string, unknown> | (string | number)[]) => string;
 
