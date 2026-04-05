96763f9bdc2c61b4e64565e9828ed8797f766524
Almighty-Shogun <alm1ghtyshogun1998@gmail.com>
2026-04-05 20:22:14 +0200

    fix: WebKit Native Bridge not building correctly

--- packages/webkit-native-bridge/tsdown.config.ts
+++ packages/webkit-native-bridge/tsdown.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-    clean: true,
-    dts: true,
     entry: ['./src/index.ts'],
-    format: ['esm'],
-    platform: 'neutral',
-})
+    minify: true
+});
