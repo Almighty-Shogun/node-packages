8b445e84fab3173a774f047bd1c3cd450c287269
Almighty-Shogun <alm1ghtyshogun1998@gmail.com>
2026-05-26 00:30:57 +0200

    feat: changed parameters and returns styling

--- docs/.vitepress/theme/components/Parameters.vue
+++ docs/.vitepress/theme/components/Parameters.vue
@@ -1,66 +1,26 @@
 <template>
-    <section v-if="parameters.length" class="frontmatter-docs__section">
-        <h2 id="parameters">Parameters</h2>
+    <h2 id="parameters">Parameters</h2>
 
-        <div class="frontmatter-docs__table-wrap">
-            <table class="frontmatter-docs__table">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th v-if="hasType">Type</th>
-                        <th>Description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="parameter in parameters" :key="parameter.name">
-                        <td>
-                            <code>{{ parameter.name }}</code>
-                        </td>
-                        <td v-if="hasType">
-                            <code v-if="parameter.type">{{ parameter.type }}</code>
-                        </td>
-                        <td>
-                            <span
-                                v-for="(part, index) in parseInline(parameter.description)"
-                                :key="index"
-                            >
-                                <code v-if="part.code">{{ part.value }}</code>
-                                <template v-else>{{ part.value }}</template>
-                            </span>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </section>
+    <p v-for="({ name, description, type, optional, defaultValue }) of parameters">
+        <code><strong>{{ name }}{{ optional ? '?' : '' }}: </strong>{{ type }}</code>
+
+        <br>
+        <span v-html="renderInlineCode(description)"/>
+
+        <template v-if="defaultValue">
+            <br>
+            <small><small><strong>Default: </strong><code>{{ defaultValue }}</code></small></small>
+        </template>
+    </p>
+
 </template>
 
 <script setup lang="ts">
 import { useData } from 'vitepress'
 import { computed, unref } from 'vue'
-
-type FrontmatterItem = {
-    name: string;
-    description: string;
-    type?: string;
-};
-
-type InlinePart = {
-    value: string;
-    code: boolean;
-};
+import { renderInlineCode } from './inlineCode'
 
 const { frontmatter } = useData();
 
-const parameters = computed<FrontmatterItem[]>(() => unref(frontmatter).params || []);
-const hasType = computed(() => parameters.value.some((parameter) => Boolean(parameter.type)));
-
-function parseInline(value: string): InlinePart[] {
-    return value.split(/(`[^`]+`)/g)
-        .filter(Boolean)
-        .map((part) => ({
-            value: part.startsWith('`') && part.endsWith('`') ? part.slice(1, -1) : part,
-            code: part.startsWith('`') && part.endsWith('`')
-        }));
-}
+const parameters = computed(() => unref(frontmatter).params || []);
 </script>
--- docs/.vitepress/theme/components/Returns.vue
+++ docs/.vitepress/theme/components/Returns.vue
@@ -1,77 +1,23 @@
 <template>
-    <section v-if="hasReturns" class="frontmatter-docs__section">
-        <h2 id="returns">Returns</h2>
+    <h2 id="returns">Returns</h2>
 
-        <p v-if="typeof returns === 'string'" class="frontmatter-docs__description">
-            <span
-                v-for="(part, index) in parseInline(returns)"
-                :key="index"
-            >
-                <code v-if="part.code">{{ part.value }}</code>
-                <template v-else>{{ part.value }}</template>
-            </span>
-        </p>
+    <span v-if="isString" v-html="renderInlineCode(returns)"/>
 
-        <div v-else class="frontmatter-docs__table-wrap">
-            <table class="frontmatter-docs__table">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th v-if="hasType">Type</th>
-                        <th>Description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="item in returns" :key="item.name">
-                        <td>
-                            <code>{{ item.name }}</code>
-                        </td>
-                        <td v-if="hasType">
-                            <code v-if="item.type">{{ item.type }}</code>
-                        </td>
-                        <td>
-                            <span
-                                v-for="(part, index) in parseInline(item.description)"
-                                :key="index"
-                            >
-                                <code v-if="part.code">{{ part.value }}</code>
-                                <template v-else>{{ part.value }}</template>
-                            </span>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </section>
+    <p v-for="({ name, description, type }) of returns" v-else>
+        <code><strong>{{ name }}</strong><template v-if="type">: {{ type }}</template></code>
+
+        <br>
+        <span v-html="renderInlineCode(description)"/>
+    </p>
 </template>
 
 <script setup lang="ts">
 import { useData } from 'vitepress'
 import { computed, unref } from 'vue'
-
-type FrontmatterItem = {
-    name: string;
-    description: string;
-    type?: string;
-};
-
-type InlinePart = {
-    value: string;
-    code: boolean;
-};
+import { renderInlineCode } from './inlineCode'
 
 const { frontmatter } = useData();
 
-const returns = computed<string | FrontmatterItem[]>(() => unref(frontmatter).returns || []);
-const hasReturns = computed(() => typeof returns.value === 'string' ? returns.value.length > 0 : returns.value.length > 0);
-const hasType = computed(() => Array.isArray(returns.value) && returns.value.some((item) => Boolean(item.type)));
-
-function parseInline(value: string): InlinePart[] {
-    return value.split(/(`[^`]+`)/g)
-        .filter(Boolean)
-        .map((part) => ({
-            value: part.startsWith('`') && part.endsWith('`') ? part.slice(1, -1) : part,
-            code: part.startsWith('`') && part.endsWith('`')
-        }));
-}
+const isString = computed(() => typeof unref(frontmatter).returns === "string");
+const returns = computed(() => unref(frontmatter).returns || (isString.value ? null : []));
 </script>
