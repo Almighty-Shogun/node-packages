906971f3635d68627d442a460ae1017d1158ecba
Shogun <alm1ghtyshogun1998@gmail.com>
2026-08-10 01:04:25 +0200

    refactor: add shared element, hot key and i18n types

--- packages/common/src/internal/types.ts
+++ packages/common/src/internal/types.ts
@@ -1,11 +1,26 @@
+import type { ComponentPublicInstance } from 'vue';
+import type { HTMLTarget, Undefinable } from '@almighty-shogun/utils';
+
+export type ComponentElement = HTMLElement | ComponentPublicInstance;
+export type ComponentTarget = HTMLTarget | ComponentPublicInstance;
+
 export type TranslationParams = Record<string, unknown> | (string | number)[];
 
 export type TranslateExists = (key: string) => boolean;
-export type Translate = (key: string, params?: TranslationParams) => string;
+export type Translate = (key: string, params?: Undefinable<TranslationParams>) => string;
 
 export type I18n = {
-    t?: Translate;
-    $t?: Translate;
-    te?: TranslateExists;
-    $te?: TranslateExists;
+    t?: Undefinable<Translate>;
+    $t?: Undefinable<Translate>;
+    te?: Undefinable<TranslateExists>;
+    $te?: Undefinable<TranslateExists>;
+};
+
+export type ParsedHotKey = {
+    key: string;
+    ctrl: boolean;
+    alt: boolean;
+    shift: boolean;
+    meta: boolean;
+    mod: boolean;
 };
